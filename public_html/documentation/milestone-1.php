fde0c06d671c3fada6682e71f833504f6dbc11c9
Diane Peshlakai <dpeshlakai3@cnm.edu>
2016-07-19 23:17:01 -0600

    added persona

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -3,11 +3,25 @@
 	<title>PWP Milestone 1</title>
 	<body>
 		<h1>Purpose, Audience & Goal</h1>
-		<p><bold>Purpose</bold>:The project is a professional portfolio website with a focus on full stack development.</p>
+		<p>Purpose: The project is a professional portfolio website with a focus on full stack development.</p>
+		<p>Audience: The site is designed to target a professional audience in the technology community such as entrepreneurs, startup founders, business owners, and web developers.</p>
+		<p>Goal: The primary goal of the site is to obtain contact with her in regards of contract work, internships, and/or employment through the contact information and on social media profiles. The second goal is to present my online projects.</p>
+		<h1>Persona</h1>
+		<p>
+			<ol>
+			<li>Name: Diane Peshlakai </li>
+			<li>Age: 26</li>
+			<li>Profession: A junior web developer in route to become an experienced full stack developer. She has done a some projects and is continually growing her portfolio.</li>
+			<li>Technology: Windows 7, and android phone</li>
+			<li>Attitudes and Behaviors: Eager to put more projects on her portfolio. Uses Twitter, Facebook and LinkedIn for business connections.</li>
+			<li>Frustrations and Needs: She would like to get a one or two personal projects near the end of her graduation with Deep Dive Coding. She's actively meeting other developers for collaborative projects by attending local meetups.</li>
+			<li>Diane is only concerned with getting more projects done before graduation on top of her other important assignments. Can she manage her time well? She needs this site to display a strong portfolio for any of the following: contract work, internship, and/or employment. </li>
+			</ol>
+		</p>
 
 	</body>
 
 
 
 
-</html>**
+</html>
