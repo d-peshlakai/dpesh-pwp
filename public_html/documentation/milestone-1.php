deff34b1ac615868af9787d50cf4bc3d8865d360
Diane Peshlakai <dpeshlakai3@cnm.edu>
2016-07-19 22:46:02 -0600

    added purpose statement

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<title>PWP Milestone 1</title>
+	<body>
+		<h1>Purpose, Audience & Goal</h1>
+		<p><bold>Purpose</bold>:The project is a professional portfolio website with a focus on full stack development.</p>
+
+	</body>
+
+
+
+
+</html>**
