a3f46c9d80bd2636c7be592f21be38413b1a1e17
Diane Peshlakai <dpeshlakai3@cnm.edu>
2016-07-20 19:03:32 -0600

    updated rough draft

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -5,24 +5,21 @@
 		<h1>Purpose, Audience & Goal</h1>
 		<p>Purpose: The project is a professional portfolio website with a focus on full stack development experience.</p>
 		<p>Audience: The site is designed to target a professional audience in the technology community such as entrepreneurs, startup founders, business owners, and web developers.</p>
-		<p>Goal: The primary goal of the site is to obtain contact with her in regards of contract work, internships, and/or employment through the contact information and on social media profiles. The second goal is to present her online projects.</p>
+		<p>Goal: The primary goal of the site is to obtain contact with Diane in regards of contract work, internships, and/or employment through the contact information and on social media profiles. The second goal is to present her online projects.</p>
 		<h1>Persona</h1>
 		<p>
 			<ol>
 			<li>Name: Diane Peshlakai </li>
 			<li>Age: 26</li>
-			<li>Profession: A junior web developer in route to become an experienced full stack developer. She has done a some projects and is continuously growing her portfolio.</li>
-			<li>Technology: Windows 7, and android phone</li>
-			<li>Attitudes and Behaviors: Eager to put more projects on her portfolio. Uses Twitter, Facebook and LinkedIn for business connections.</li>
+			<li>Profession: A junior web developer in route to become an experienced full stack developer. She has done some projects and is continuously growing her portfolio of web sites.</li>
+			<li>Technology: Windows 7, and android phone which requires mobile friendly features.</li>
+			<li>Attitudes and Behaviors: She's eager to put more web projects on her portfolio this coming fall of 2016. Uses Twitter, Facebook and LinkedIn for business connections in Albuquerque, NM and surrounding states such as Colorado.</li>
 			<li>Frustrations and Needs: She would like to get a one or two personal projects near the end of her graduation with Deep Dive Coding. She's actively meeting other developers for collaborative projects by attending local meetups.</li>
 			<li>Diane is only concerned with getting more projects done before graduation on top of her other important assignments. Can she manage her time well? She needs this site to display a strong portfolio for any of the following: contract work, full-time internship, and/or employment. </li>
-			<h1>Use Case</h1>
 			</ol>
-		</p>
+			<h1>Use Case</h1>
+			<p>Diane is going to manage her time carefully to complete side projects including all assignments upon graduation. She wants to present a strong portfolio on her web site to the technology community in Albuquerque, New Mexico. Her goal is to be an experienced full stack developer with a professional background. She needs a mobile friendly and desktop responsive website. It's 2:48pm and she is managing a time schedule to work on school assignments and other side projects before fall of 2016 arrives. She's actively seeking on social media for job openings and local networking groups to prepare herself for employment. In her ten week school course, she is perfecting time management and paying close attention to the topics assigned. Focus and hardwork are on the to do list to make the site a professional portfolio.</p>
+			</p>
 
 	</body>
-
-
-
-
 </html>
