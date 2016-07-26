95f60139effe3f52474509596b7ab113db774155
Diane Peshlakai <dpeshlakai3@cnm.edu>
2016-07-26 16:42:07 -0600

    updated persona and use case

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -5,21 +5,20 @@
 		<h1>Purpose, Audience & Goal</h1>
 		<p>Purpose: The project is a professional portfolio website with a focus on full stack development experience.</p>
 		<p>Audience: The site is designed to target a professional audience in the technology community such as entrepreneurs, startup founders, business owners, and web developers.</p>
-		<p>Goal: The primary goal of the site is to obtain contact with Diane in regards of contract work, internships, and/or employment through the contact information and on social media profiles. The second goal is to present her online projects.</p>
+		<p>Goal: The primary goal of the site is to obtain contact with local web developement companies, small business and tech startups in regards of contract work, internships, and/or employment through the contact information and on social media profiles.</p>
 		<h1>Persona</h1>
 		<p>
 			<ol>
-			<li>Name: Diane Peshlakai </li>
+			<li>Name: Johnny Depp </li>
 			<li>Age: 26</li>
-			<li>Profession: A junior web developer in route to become an experienced full stack developer. She has done some projects and is continuously growing her portfolio of web sites.</li>
-			<li>Technology: Windows 7, and android phone which requires mobile friendly features.</li>
-			<li>Attitudes and Behaviors: She's eager to put more web projects on her portfolio this coming fall of 2016. Uses Twitter, Facebook and LinkedIn for business connections in Albuquerque, NM and surrounding states such as Colorado.</li>
-			<li>Frustrations and Needs: She would like to get a one or two personal projects near the end of her graduation with Deep Dive Coding. She's actively meeting other developers for collaborative projects by attending local meetups.</li>
-			<li>Diane is only concerned with getting more projects done before graduation on top of her other important assignments. Can she manage her time well? She needs this site to display a strong portfolio for any of the following: contract work, full-time internship, and/or employment. </li>
+			<li>Profession: He is a the lead web developer for the company Lavu as a full stack web developer. He has been in the technology communitity for 11 years. He also has been running the team at Lavu for 5 years. He is looking  to hire a junior web developer.</li>
+			<li>Technology: Mr. Depp carries a LG G6 android phone at all times for meetings and uses a HP windows 7 desktop daily for work .</li>
+			<li>Attitudes and Behaviors: He enjoys his job and loves the field web developement. He enjoys teaching and leading his team to make the Lavu web application a success. He wants to maintain a strong creative team. He uses to social media, and attends the tech meetups to meet with future web developers for the Albuquerque company.</li>
+			<li>Frustrations and Needs: He would like to set up interviews in the next week. He's actively attending tech events to inform his job opening. Time is his enemy because the company needs an employer before the week ends .</li>
+			<li>Johnny is only concerned with getting a web developer in time. Can he manage his time well? While managing a large team along with weekly meetings, he must make the time to hire a web developer. </li>
 			</ol>
 			<h1>Use Case</h1>
-			<p>Diane is going to manage her time carefully to complete side projects including all assignments upon graduation. She wants to present a strong portfolio on her web site to the technology community in Albuquerque, New Mexico. Her goal is to be an experienced full stack developer with a professional background. She needs a mobile friendly and desktop responsive website. It's 2:48pm and she is managing a time schedule to work on school assignments and other side projects before fall of 2016 arrives. She's actively seeking on social media for job openings and local networking groups to prepare herself for employment. In her ten week school course, she is perfecting time management and paying close attention to the topics assigned. Focus and hardwork are on the to do list to make the site a professional portfolio.</p>
-			</p>
+			<p>Johnny Depp is going to manage his time carefully while managing the team at Lavu. He's looking for a junior web developer for his team at Lavu in Albuquerque, New Mexico with advanced skills and with a professional website. His goal is to hire a junior web developer who knows the languages Javascript, PHP, HTML/CSS and Angular 2. When he heard that the Deep Dive Coding Bootcamp was graduating this week he attended the demo/employer day to potentially hire a developer. It's 2:48pm, two days after employer day he notices a resume by Diane Peshlakai who graduated from Deep Dive Coding and goes further to schedule an interview. </p>
 
 	</body>
 </html>
