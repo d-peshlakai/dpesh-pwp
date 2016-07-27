8850d024ec3a301cc654c7f369420dacc0dd345c
Diane Peshlakai <dpeshlakai3@cnm.edu>
2016-07-26 21:17:12 -0600

    more input in the content strategy and finished contact content

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -13,5 +13,14 @@
 	<li>Contact</li>
 	</ol>
 	<h2>Content Strategy</h2>
-	<p><strong>Home Page</strong></p>
+	<p><strong>Home Page</strong>: This webpage will have an image symbolizing where I'm from. In the welcome context box
+		it will have information stating my profession as a web developer and the services focused on web development. It
+		will feature three calls of action for the user to be routed to About Me page, Portfolio, and Contact page.</p>
+	<p><strong>About Me</strong>: This page will explain my professional history, skills, and career highlights. This
+		page will show my personality. It will mostly contain content explaining to employers and businesses services I
+		provide. This page will also include social media links for professional business connections.</p>
+	<p><strong>Portfolio</strong>: This page will mostly contain a gallery of images. The images will be my projects
+	that I have done. It will also include a brief description about my roles in each project.</p>
+	<p><strong>Contact</strong></p>: This page will have a contact form. Next to the contact form will include email
+	address, and social media links, as well as GitHub/Linked In profiles. 
 </html>
