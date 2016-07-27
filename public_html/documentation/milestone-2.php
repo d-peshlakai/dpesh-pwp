f83cccc0a2bd710d0cd494a64287fee3f6134d3d
Diane Peshlakai <dpeshlakai3@cnm.edu>
2016-07-26 20:50:55 -0600

    finished sitemap info and starting content strategy

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<meta charset="UTF-8">
+	<title>DianePeshlakai.com</title>
+	<h1>Personal Website: Diane Peshlakai </h1>
+	<h2>Sitemap</h2>
+	<p>The sitemap will contain four top level pages. There will not be any sub-navigation or sub-pages.
+		This will be the Minimal Viable Product.</p>
+	<ol>
+		<li>Homepage</li>
+	<li>About Me</li>
+	<li>Porfolio</li>
+	<li>Contact</li>
+	</ol>
+	<h2>Content Strategy</h2>
+	<p><strong>Home Page</strong></p>
+</html>
